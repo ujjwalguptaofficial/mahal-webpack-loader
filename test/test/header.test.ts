f1cf5fa5207dacaf1241bca4c2719377a797d455
Ujjwal Gupta <ujjwalkumargupta4u@gmail.com>
2022-02-27 08:57:01 +0530

    add test for checking styles changes with scoped and without scoped

---
+++ test/test/header.test.ts
@@ -0,0 +1,59 @@
+import { initiate, mount } from "mahal-test-utils";
+import Header from "@/components/header.mahal";
+import { expect } from "chai";
+import { app } from "@/index";
+
+describe("Mahal Intro", () => {
+    let component: Header;
+    before(async () => {
+        component = await mount.call(app, Header);
+    });
+
+    it("check h1 computed color", () => {
+        const h1 = component.find('.header1');
+        expect(h1.innerHTML).equal(`Header`);
+        expect(window.getComputedStyle(h1).color).equal('red');
+    });
+
+    it('check for h1 scoped', (done) => {
+        const el = component.find('.header1');
+        let isFound = false;
+        for (var i = 0, atts = el.attributes, n = atts.length; i < n; i++) {
+            const attr = atts[i];
+            console.log('att', attr.nodeName);
+            if (attr.nodeName.includes('mahal-')) {
+                isFound = true;
+            }
+        }
+        if (isFound) {
+            done("scoped attr found");
+        }
+        else {
+            done();
+        }
+    });
+
+    it("check h2 computed color", () => {
+        const h1 = component.find('.header2');
+        expect(h1.innerHTML).equal(`Header`);
+        expect(window.getComputedStyle(h1).color).equal('blue');
+    });
+
+    it('check for h2 scoped', (done) => {
+        const el = component.find('.header2');
+        let isFound = false;
+        for (var i = 0, atts = el.attributes, n = atts.length; i < n; i++) {
+            const attr = atts[i];
+            console.log('att', attr.nodeName);
+            if (attr.nodeName.includes('mahal-')) {
+                isFound = true;
+            }
+        }
+        if (!isFound) {
+            done("scoped attr found");
+        }
+        else {
+            done();
+        }
+    });
+});
