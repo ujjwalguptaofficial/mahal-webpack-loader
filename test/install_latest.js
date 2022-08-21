bac32617ad6bbd85cdb4c50dc75a734ec13e0eb8
Ujjwal Gupta <ujjwalkumargupta44@gmail.com>
2022-08-21 21:49:41 +0530

    release v - 1.4.2

---
+++ test/install_latest.js
@@ -0,0 +1,6 @@
+const { readFileSync } = require('fs');
+const { execSync } = require('child_process');
+const path = require('path');
+
+execSync(`npm i mahal@latest`);
+execSync(`npm i mahal-test-utils@latest`);
